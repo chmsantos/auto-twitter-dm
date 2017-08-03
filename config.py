7fd73e1f131913b79d583f1f948a3ae4da6a6f3e
chmsantos <30699695+chmsantos@users.noreply.github.com>
2017-08-04 01:08:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ def generate_dm_text(name):
 	return '''Hey {}, I hope you're having a nice day today and I sincerely want you to know that 
 			you are loved and you are always worth it. You deserve everything my friend. :)'''.format(name)
 
-scheduler_time = 15 #in minutes
+scheduler_time = 1 #in minutes
 
-tw_username = "paraazz" #change this to yours
+tw_username = "imcharliesantos" #change this to yours
