eaabf25a51a14d939731d277ef46629fcf366ad9
chmsantos <30699695+chmsantos@users.noreply.github.com>
2017-08-04 00:51:15 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# configure the dm text
+def generate_dm_text(name):
+	return '''Hey {}, I hope you're having a nice day today and I sincerely want you to know that 
+			you are loved and you are always worth it. You deserve everything my friend. :)'''.format(name)
+
+scheduler_time = 15 #in minutes
+
+tw_username = "paraazz" #change this to yours
